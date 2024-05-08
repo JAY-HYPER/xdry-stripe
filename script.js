7ab7eb0d44b0c08e220b334fa47eb8edabda5c7d
JAY-HYPER <168636283+JAY-HYPER@users.noreply.github.com>
2024-05-08 17:36:54 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeImageA(filename) {
+  let img = document.querySelector('.main');
+  img.setAttribute("src", filename);
+}
+function changeImageB(filename) {
+  let img = document.querySelector('.sideColor');
+  img.setAttribute("src", filename);
+}
+function changeImageC(filename) {
+  let img = document.querySelector('.strA');
+  img.setAttribute("src", filename);
+}
+function changeImageD(filename) {
+  let img = document.querySelector('.strB');
+  img.setAttribute("src", filename);
+}
